90ca036a04943c2af26d38979c517accfeb1204f
helenohyeah <ouyang.helen@gmail.com>
2020-11-09 14:14:20 +0000

    Init commit

---
+++ math-game.rb
@@ -0,0 +1,20 @@
+class Player
+  # tracks player number for turn order
+  # tracks life ("score")
+end
+
+class Math_Question
+  # generate math problem
+end
+
+class Announcer
+  # asks question
+  # announces results
+  # announces score
+  # announces game over
+end
+
+class Game
+  # tracks turn order (current player)
+  # tracks overall game logic and order (ask, wait for ansewer etc)
+end
